cbbe633bae16611648b01ed0e4ca2e1f6d469ad9
lindaRamosGalindo <leramos40@misena.edu.co>
2016-11-02 14:09:23 -0500

    .
    .

--- lgm/lgm/apps/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns,url
-
-
-urlpatterns = patterns('lgm.apps.home.views',
-		url(r'^$','index_view', name = 'vista_principal'),
-		url(r'^add/clase/$','add_clase_view', name = 'vista_agregar_clase'),
-)
-
